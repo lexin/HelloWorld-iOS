59fc9cf7f9f4c6cc08e7b901bc2fa65e19cacecb
Oleksii Romanko <o.roman'ko@relevant.software>
2019-07-19 15:15:43 +0300

    initial

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Alexey Romanko on 7/19/19.
+//  Copyright © 2019 Romanko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
